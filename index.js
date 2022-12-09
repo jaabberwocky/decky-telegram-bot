fc7f69a7510588c091f027b6c9339a539de9d07b
tobias <tobias.leong@zendesk.com>
2022-12-09 11:58:44 +0800

    Refactor to new command /register

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const bot = new TelegramBot(token, { polling: true });
 let storage = {};
 console.log("Initialised database storage...");
 
+// register
 bot.onText(/\/start/, async (msg) => {
+    bot.sendMessage(msg.chat.id, "Hello and welcome to Decky! This simple bot helps you remember which deck you have parked your car at.\n\n <b>Commands</b>\n/register: Register here first\n/park: Once registered, park your car here", {parse_mode: "HTML"});
+})
+
+bot.onText(/\/register/, async (msg) => {
     if (msg.from.id in storage) {
         bot.sendMessage(msg.chat.id, `You are registered! Carpark deck location: ${storage[msg.from.id]}`);
     } else {
@@ -18,10 +23,23 @@ bot.onText(/\/start/, async (msg) => {
                 "force_reply": true
             }
         });
-        bot.onReplyToMessage(msg.chat.id, locationPrompt.message_id, (msg) => {
-            storage[msg.from.id] = msg.text;
-            console.log(`Stored value ${msg.from.id}[${msg.text}]`);
-            bot.sendMessage(msg.chat.id, `I've registered you with location ${msg.text}!`);
-        })    
+        bot.onReplyToMessage(msg.chat.id, locationPrompt.message_id, async (msg) => {
+            const location = msg.text;
+            const deckPrompt = await bot.sendMessage(msg.chat.id, `Got it! Enter the decks/levels of your carpark separated by a space. Example: 1A 1B 2A 2B`, {
+                "reply_markup": {
+                    "force_reply": true
+                }
+            })
+            bot.onReplyToMessage(msg.chat.id, deckPrompt.message_id, async (msg) => {
+                const decks = msg.text.split(" ");
+                console.log(decks);
+                storage[msg.from.id] = {
+                    "location": location,
+                    decks: decks
+                };
+                console.log(storage);
+                bot.sendMessage(msg.chat.id, "You are now registered!");
+            })
+        })
     }
 })
